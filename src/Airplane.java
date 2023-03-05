f0cf8bb26928f3cd6552c2a30582492a1fe1ba02
LapinKA <lapinkirill.apple@mail.ru>
2023-03-05 17:05:10 +1200

    Initial commit

--- src/Airplane.java
+++ src/Airplane.java
@@ -1,10 +1,11 @@
-public class Airplane {
-  int capacity;
-  int weight;
-}
-class Mai{
-  public static void main(String[] args) {
+class Reader{
+  int age;
+  public void takeBook(String bookName){
+    System.out.println("Взял " + bookName + " книгу");
+  }
 
+  public static void main(String[] args) {
+    Reader aboba = new Reader();
   }
 }
 
--- src/Mouse.java
+++ src/Mouse.java
@@ -1,18 +1,6 @@
 public class Mouse {
-  String name;
-  String color;
-  int bodyL;
-  int tailL;
-  int age;
-  public void eatCheese(){
-    System.out.println("Я " + name + " съел сыр");
-  }
 }
 class Main{
-  public static void main(String[] args) {
-    Mouse jerry = new Mouse();
-    jerry.name = "Jerry";
-    jerry.eatCheese();
-  }
+
 }
 
